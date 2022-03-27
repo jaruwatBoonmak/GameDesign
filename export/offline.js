68b989759e0bd302f71c01f94548183db1a42e04
JB <58852257+jaruwatBoonmak@users.noreply.github.com>
2022-03-28 00:05:42 +0700

    first

---
+++ export/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1648400615,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/player-sheet3.png",
+		"images/ground.png",
+		"images/coin-sheet0.png",
+		"images/bg.png",
+		"images/sprite-sheet0.png",
+		"images/enmy-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/wingate-sheet0.png",
+		"images/startb-sheet0.png",
+		"images/playstart-sheet0.png",
+		"images/playstart-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
